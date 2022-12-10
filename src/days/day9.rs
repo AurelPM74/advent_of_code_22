182fc38cc4bff503bc94cd47b0e147d2d720bce8
AurelPM74 <68423666+AurelPM74@users.noreply.github.com>
2022-12-10 22:59:14 +0100

    Day 9 second part

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -1,15 +1,18 @@
 use crate::utils::{self, read_input_of_the_day_lines};
 use std::fmt;
 
-//Struct of the rope: it has a current head position, a current tail position, a list of position visited
+//Struct of the rope: it has a current head position, a current tail position, a list of position visited, a list of knots
 struct Rope{
-    head: (i64, i64),
-    tail: (i64, i64),
+    knots: Vec<(i64, i64)>,
+    //shoulf be the number ofknots without the head
+    len: usize,
     //We will keep track of the positions visited as a matrix 1000*1000
     //We assume that that will be enough, the starting point will be (500, 500) 
     positions: Vec<Vec<u64>>,
     visited: u64
 }
+
+
 //Method of the rope
 impl Rope{
 
@@ -30,50 +33,52 @@ impl Rope{
         //We move the head accordingly, for each step we update the tail position:
         for i in 1..=instruction.1{
             //Move head one step
-            self.head = (self.head.0 + to_move_head.0, self.head.1 + to_move_head.1);
+            self.knots[0] = (self.knots[0].0 + to_move_head.0, self.knots[0].1 + to_move_head.1);
 
-            //update the tail position
-            self.update_tail_position();
+            for i in 1..=self.len{
+                //update the position of the n-th segment
+                self.update_position(i);
+
+                //println!("{}", instruction.0);
+                //for line in self.matrix() {
+                //    println!("{:?}", line);
+                //}
+                //println!("     ");
+                }
 
             //update the matrix of positions visited
             self.update_position_visited();
-
-            //println!("{}", instruction.0);
-            //for line in self.matrix() {
-            //    println!("{:?}", line);
-            //}
-            //println!("     ");
         }
         
     }
 
-    //Update the position of the tail after a move of one of the head
-    fn  update_tail_position(&mut self) {
+    //Update the position of the n_segment after a move
+    fn  update_position(&mut self, i: usize) {
 
         //If the head and tail are in the same "radius" of one we do nothing:
-        if (self.head.0 - self.tail.0).abs() > 1 || (self.head.1 - self.tail.1).abs() > 1{
+        if (self.knots[i-1].0 - self.knots[i].0).abs() > 1 || (self.knots[i-1].1 - self.knots[i].1).abs() > 1{
 
             //in which direction lateraly we need to move the tail, zero if above/below
-            let lat_dir = self.tail.1 - self.head.1;
+            let lat_dir = self.knots[i].1 - self.knots[i-1].1;
 
             //in which direction verticaly we need to move the tail, zero if left right
-            let vert_dir = self.tail.0 - self.head.0;
+            let vert_dir = self.knots[i].0 - self.knots[i-1].0;
 
             //Case were the head is at the same level of the head 
             if vert_dir == 0 {
 
-                self.tail.1 = self.tail.1 - lat_dir/lat_dir.abs()
+                self.knots[i].1 = self.knots[i].1 - lat_dir/lat_dir.abs()
 
             } else {
 
                 //The head is above or below the tail, not directly diagonal
                 //in any case we move the head up/down 
-                self.tail.0 = self.tail.0 - vert_dir/vert_dir.abs();
+                self.knots[i].0 = self.knots[i].0 - vert_dir/vert_dir.abs();
 
                 //and the tail move lateraly in the direction of the head if the head isn't just above
                 if lat_dir != 0 {
                     //move diag
-                    self.tail.1 = self.tail.1 - lat_dir/lat_dir.abs();
+                    self.knots[i].1 = self.knots[i].1 - lat_dir/lat_dir.abs();
                 }
                 
             }
@@ -83,8 +88,8 @@ impl Rope{
     //Update the matrix of visited position
     fn update_position_visited(&mut self){
         //Check if the tail is in and unvisited case, if it is we change the matrix
-        if self.positions[self.tail.0.abs() as usize][self.tail.1.abs() as usize] == 0 {
-            self.positions[self.tail.0.abs() as usize][self.tail.1.abs() as usize] = 1;
+        if self.positions[self.knots[self.len].0.abs() as usize][self.knots[self.len].1.abs() as usize] == 0 {
+            self.positions[self.knots[self.len].0.abs() as usize][self.knots[self.len].1.abs() as usize] = 1;
             self.visited += 1
         }
     }
@@ -92,8 +97,8 @@ impl Rope{
     fn matrix(&self) -> Vec<Vec<i64>>{
         let len = self.positions[0].len();
         let mut matrix = std::vec::from_elem(std::vec::from_elem(0, len), len);
-        matrix[self.tail.0.abs() as usize][self.tail.1.abs() as usize] = 2;
-        matrix[self.head.0.abs() as usize][self.head.1.abs() as usize] = 1;
+        matrix[self.knots[self.len].0.abs() as usize][self.knots[self.len].1.abs() as usize] = 2;
+        matrix[self.knots[0].0.abs() as usize][self.knots[0].1.abs() as usize] = 1;
         matrix
     }
 }
@@ -117,12 +122,13 @@ pub fn solve_1() -> u64 {
     //Parse the input
     let instruction_chars:Vec<Vec<String>> = instruction_string.iter().map(|str| str.split_whitespace().map(String::from).collect()).collect();
     let instructions:Vec<(char, u64)> = instruction_chars.iter().map(|string| (string[0].parse::<char>().unwrap(),string[1].parse::<u64>().unwrap())).collect();
-    println!("{:?}", instructions.len());
+    
+    let starting_position = std::vec::from_elem((500, 500), 2);
 
     //initialize the rope, it start in the middle of a 2000*2000 grid
     let mut rope_simulation = Rope {
-        head: (500, 500),
-        tail: (500, 500),
+        knots: starting_position,
+        len: 1,
         positions: std::vec::from_elem(std::vec::from_elem(0 as u64, 1000), 1000),
         visited: 0
     };
@@ -140,8 +146,33 @@ pub fn solve_1() -> u64 {
 }
 
 pub fn solve_2() -> u64 {
+    //load the inputs 
+    let instruction_string = read_input_of_the_day_lines(9);
 
-    0
+    //Parse the input
+    let instruction_chars:Vec<Vec<String>> = instruction_string.iter().map(|str| str.split_whitespace().map(String::from).collect()).collect();
+    let instructions:Vec<(char, u64)> = instruction_chars.iter().map(|string| (string[0].parse::<char>().unwrap(),string[1].parse::<u64>().unwrap())).collect();
+    
+    let starting_position = std::vec::from_elem((500, 500), 10);
+
+    //initialize the rope, it start in the middle of a 2000*2000 grid
+    let mut rope_simulation = Rope {
+        knots: starting_position,
+        len: 9,
+        positions: std::vec::from_elem(std::vec::from_elem(0 as u64, 1000), 1000),
+        visited: 0
+    };
+
+    //Add the starting position to the matrix of visited position
+    rope_simulation.update_position_visited();
+
+    //Simulate
+    for ins in instructions{
+        rope_simulation.next_instruction(ins)
+    }
+
+    return  rope_simulation.visited;
+
 }
 
 mod tests {
@@ -157,24 +188,26 @@ mod tests {
         let position = std::vec::from_elem(std::vec::from_elem(0 as u64, 6), 5);
 
         let mut ropes = Rope {
-            head: (4, 0),
-            tail: (4, 0),
+            knots: vec![(4, 0),(4, 0)],
+            len: 1,
             positions: position,
             visited: 0
         };
-            let instructions = [('U', 1),
-                                ('R', 1),
-                                ('U', 1),
+            let instructions = [('R', 4),
+                                ('U', 4),
+                                ('L', 3),
                                 ('D', 1),
                                 ('R', 4),
                                 ('D', 1),
                                 ('L', 5),
                                 ('R', 2)];
-        //let instructions = [('U', 1),('L', 2)];
+        println!("a");
         for ins in instructions{
+            println!("b");
             ropes.next_instruction(ins);
         }
         let case_visited: u64 = ropes.positions.iter().map(|vec| vec.iter().sum::<u64>()).sum();
+        println!("c");
         assert_eq!(case_visited, 13)
     }
 }
